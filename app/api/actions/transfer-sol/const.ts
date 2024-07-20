b612fe8544e59e125a3053c0cf2c2219a0c27f95
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-07-21 02:36:41 +0800

    feat: transfer-sol action

---
+++ app/api/actions/transfer-sol/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "EQb8LApPTtZFk3cY7WcaAmEvuL6s3Q1q8ozxcPcBJ5dc", // devnet wallet
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
